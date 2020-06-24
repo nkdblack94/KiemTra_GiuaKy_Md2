4a853f1ec05402cc92a2c3cd40538bf76d00906c
nkdblack94 <nkdblack1294@gmail.com>
2020-06-24 22:44:38 +0700

    Quản lý sinh viên

--- src/main/java/RunStudentClass.java
+++ src/main/java/RunStudentClass.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
-public class RunStudentClass extends StudentClass {
+public class RunStudentClass extends Student {
 
     public static void main(String[] args) {
-        RunStudentClass studentClass = new RunStudentClass();
+        StudentClass students = new StudentClass();
         Scanner input = new Scanner(System.in);
         int choose;
 
         do {
             System.out.println("Choose a function");
-            System.out.println("1. Display list");
+            System.out.println("1. Print the student list");
             System.out.println("2. Add to the list");
-            System.out.println("3. Delete from list");
+            System.out.println("3. Edited by id");
+            System.out.println("4. Search by name");
+            System.out.println("5. Search by id");
+            System.out.println("6. Delete by id");
             System.out.println("0. Exit!");
             choose = input.nextInt();
 
             switch (choose) {
                 case 1:
-                    System.out.println("Student list");
-                    studentClass.displayStudentList();
-                    break;
+                    students.displayStudentList(); break;
                 case 2:
-                    studentClass.addNewStudent();
-                    break;
+                    students.addNewStudent(); break;
                 case 3:
-                    studentClass.removeStudent();
-                    break;
+                    students.checkEditId(); break;
+                case 4:
+                    students.searchName(); break;
+                case 5:
+                    students.searchId(); break;
+                case 6:
+                    students.deleteById();
             }
         } while (choose != 0);
     }
--- src/main/java/StudentClass.java
+++ src/main/java/StudentClass.java
@@ -3,28 +3,90 @@ import java.util.List;
 import java.util.Scanner;
 
 public class StudentClass {
-    private List<Student> studentList = new ArrayList<Student>();
+    private List<Student> students = new ArrayList<Student>();
 
-    public void displayStudentList (){
-        for (int i = 0; i < studentList.size(); i++){
-            System.out.println( i +". Name: " + studentList.get(i).getName() + ", Aeg = " + studentList.get(i).getAeg());
+    Scanner scanner = new Scanner(System.in);
+
+    public void displayStudentList() {
+        System.out.println("Student list");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(i + 1 + ". ID = " + students.get(i).getId() + ", Name: " + students.get(i).getName() + ", Age = " + students.get(i).getAge());
         }
     }
-    public void addNewStudent (){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter name");
-        String name = input.nextLine();
-        System.out.println("Enter aeg");
-        int aeg = input.nextInt();
-        Student student = new Student(name, aeg);
-        studentList.add(student);
+
+    public void addNewStudent() {
+        System.out.println("Enter id:");
+        int id = scanner.nextInt();
+        System.out.println("Enter name:");
+        String name = scanner.next();
+        System.out.println("Enter age:");
+        int age = scanner.nextInt();
+        Student student = new Student(id, name, age);
+        students.add(student);
     }
-    public void removeStudent (){
-        System.out.println("Select the component you want to delete");
-        displayStudentList();
-        Scanner input = new Scanner(System.in);
-        int index = input.nextInt();
-        Student result = studentList.remove(index);
-        System.out.println("delete successful: Name: " + result.getName() + ", Aeg = " + result.getAeg());
+
+    public void checkEditId() {
+        System.out.println("Enter the id to edit");
+        int id = scanner.nextInt();
+        for (int i = 0; i < students.size(); i++) {
+            if (id == students.get(i).getId()) {
+                System.out.println("Enter edit id");
+                int idEdit = scanner.nextInt();
+                students.get(i).setId(idEdit);
+                System.out.println("Enter edit name");
+                String name = scanner.next();
+                students.get(i).setName(name);
+                System.out.println("Enter edit age");
+                int age = scanner.nextInt();
+                students.get(i).setAge(age);
+            }
+        }
+    }
+
+    public void deleteById() {
+        System.out.println("Enter the id to delete");
+        int id = scanner.nextInt();
+        Student student = null;
+        for (int i = 0; i < students.size(); i++) {
+            if (id == students.get(i).getId()) {
+                student = students.get(i);
+            }
+        }
+        if (student != null) {
+            students.remove(student);
+            System.out.println("delete successful");
+        } else {
+            throw new IndexOutOfBoundsException("id not found");
+        }
+    }
+
+    public void searchName() {
+        System.out.println("Enter the name to search");
+        String name = scanner.next();
+        boolean check = false;
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getName().equals(name)) {
+                System.out.println(i + 1 + ". ID = " + students.get(i).getId() + ", Name: " + students.get(i).getName() + ", Age = " + students.get(i).getAge());
+                check = true;
+            }
+        }
+        if (!check) {
+            throw new IndexOutOfBoundsException("There is no name in the list");
+        }
+    }
+
+    public void searchId() {
+        System.out.println("Enter the id to search");
+        int id = scanner.nextInt();
+        boolean check = false;
+        for (int i = 0; i < students.size(); i++) {
+            if (id == students.get(i).getId()) {
+                System.out.println(i + 1 + ". ID = " + students.get(i).getId() + ", Name: " + students.get(i).getName() + ", Age = " + students.get(i).getAge());
+                check = true;
+            }
+        }
+        if (!check) {
+            throw new IndexOutOfBoundsException("id not found");
+        }
     }
 }
